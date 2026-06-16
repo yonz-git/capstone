c45ae7c76dca5692fd24a85123840f84fb564376
yonz-git <yonzelle@naver.com>
2026-06-16 13:19:20 +0200

    Update pages/index.js
    Co-authored-by: FelineHuhn <112863528+FelineHuhn@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import NoProfileHome from "../components/NoProfileHome/NoProfileHome";
 
