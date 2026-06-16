7c9f19c99205101c8ef15b7e772f29b126463c40
yonz-git <yonzelle@naver.com>
2026-06-16 13:18:34 +0200

    Update pages/index.js
    Co-authored-by: FelineHuhn <112863528+FelineHuhn@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -10,5 +10,5 @@ export default function Home() {
   }
 
   // Default state when first landing when there is no profile
-  return <NoProfileHome Onboarding={handleOnboarding} />;
+  return <NoProfileHome onOnboarding={handleOnboarding} />;
 }
